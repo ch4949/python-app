8d84b1e49915cfce65a5c44d5045d6b37bdd4611
хао чэнь <1817816954@qq.com>
2025-02-25 17:34:04 +0300

    Fixed flake8 errors in testapp.py

--- testapp.py
+++ testapp.py
@@ -1,10 +1,13 @@
-# test_app.py
 import unittest
 from app import add
 
+
 class TestAddFunction(unittest.TestCase):
+
     def test_add(self):
         self.assertEqual(add(1, 2), 3)
 
+
 if __name__ == '__main__':
     unittest.main()
+
